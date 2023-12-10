32257c7fe42f2c3c45a9476d65a6218d6accf193
Jashwanth <153367925+Jashwanth2808@users.noreply.github.com>
2023-12-10 18:23:51 +0530

    Add files via upload

---
+++ package com.java
@@ -0,0 +1,19 @@
+
+public class main
+{
+	
+	
+	public static void main(String[] args) 
+    {
+		System.out.println("Name: Jashwanth");
+        System.out.println("Father's Name: Srinivas Rao");
+        System.out.println("Mother's Name: Kamala");
+        System.out.println("Age: 18");
+        System.out.println("Gender: Male");
+        System.out.println("Address: Hyderabad");
+        System.out.println("Mobile No: 9347434909");
+		
+		
+	}
+}
+
