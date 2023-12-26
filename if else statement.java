648f827bb78cd4fa7f997558d447ed12262427f6
Jashwanth <153367925+Jashwanth2808@users.noreply.github.com>
2023-12-26 14:45:37 +0530

    Add files via upload

---
+++ if else statement.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String city = "Mumbai";
+
+        if (city.equals("Mumbai")) {
+            System.out.println("Financial city");
+        } else if (city.equals("Kolkata")) {
+            System.out.println("City of Joy");
+        } else if (city.equals("Delhi")) {
+            System.out.println("Capital of the country");
+        } else if (city.equals("Bangalore")) {
+            System.out.println("Cyber City");
+        } else {
+            System.out.println("May be Other Indian City");
+        }
+    }
+}
